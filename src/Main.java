05aae20d9322109818a3ae2bd5fb8c71d420c76a
Vitor <joao.vitor2808@outlook.com>
2024-04-24 18:53:34 -0300

    Commit File

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,22 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> num = new ArrayList<>(10);
         Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> numeros = new ArrayList<>();
 
         for (int i = 0; i < 10; i++){
             int input = sc.nextInt();
             num.add(input);
         }
-        for (int i = num.size() -1;i >= 0; i--){
-            num.get(i);
-            if (i % 2 == 0){
-                num.remove(i);
+        for (int vitor : num){
+            if (vitor % 2 == 0){
+                numeros.add(vitor);
             }
-
         }
-        System.out.print("numeros pares: "+ num);
 
-        if (num.isEmpty()){
+        if (numeros.isEmpty()){
             System.out.println("A pilha está vazia");
+        }else {
+            System.out.print("numeros pares: "+ numeros);
         }
 
     }
